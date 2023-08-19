b1d34946bf3d48c17ce0456f850b3cd791738076
Tarun <tarunsachan1996@gmail.com>
2023-08-19 20:56:00 +0530

    added backend server url

--- client/src/components/ConnectionCard.js
+++ client/src/components/ConnectionCard.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const ConnectionCard = (props) => {
-  const onDeleteHandler = (e) => {
-    e.preventDefault();
-  };
+  // const onDeleteHandler = (e) => {
+  //   e.preventDefault();
+  // };
 
   return (
     <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
